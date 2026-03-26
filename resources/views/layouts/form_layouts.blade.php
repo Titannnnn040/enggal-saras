80aba87afae82c25c5ca6bbf6fd025a3cc80ee7c
Titan <titan.kantata.gitlab@gmail.com>
2026-03-26 19:10:38 +0700

    update

--- resources/views/layouts/form_layouts.blade.php
+++ resources/views/layouts/form_layouts.blade.php
@@ -23,7 +23,7 @@
   <link rel="icon" href="/img/enggal.gif" type="image/x-icon">
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" /> 
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
-  <link id="pagestyle" href="/css/dashboard/material-dashboard.css" rel="stylesheet" />
+  <link id="pagestyle" href="//public/css/dashboard/material-dashboard.css" rel="stylesheet" />
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="/css/bootstrap/dist/css/bootstrap.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
